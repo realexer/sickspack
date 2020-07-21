fcd57bf253db46cb84e8126b63d3549ff437ac42
Alexey Skrypnik <unrealvsrealer@gmail.com>
2020-07-21 18:28:22 +0300

    Languages order and meta data in comments.

--- multilang/langs/languages.js
+++ multilang/langs/languages.js
@@ -1,31 +1,32 @@
 export default {
-	uk: 'Українська',
-	ru: 'Русский',
-	en: 'English',
-	es: 'Espaniol',
-	it: 'Italiano',
-	fr: 'Français',
-	pt: 'Português',
-	zh: '中文',
-	el: 'ελληνικά',
-	ko: '한국어',
-	pl: 'Polszczyzna',
-	ro: 'Română',
-	sv: 'Svenska',
-	tk: 'Türkmen',
-	tt: 'Татар теле',
-	cs: 'Čeština',
-	nl: 'Nederlands',
-	et: 'Eesti',
-	fi: 'Suomi',
-	de: 'Deutsch',
-	ht: 'Kreyòl ayisyen',
-	hi: 'हिन्दी, हिंदी',
-	hu: 'Magyar',
-	ga: 'Gaeilge',
-	ja: '日本語',
-	kk: 'Қазақ',
-	mk: 'Македонски јазик',
-	no: 'Norsk',
-	he: 'עברית'
+	en: 'English', // English, 360–400 million
+	es: 'Español', // Spanish, 483 million native speakers
+	it: 'Italiano', // Italian, 85 million total speakers, Italy, Switzerland, San Marino, Vatican City, Slovene Istria (Slovenia), Istria County (Croatia)
+	fr: 'Français', // French, 76.8 million worldwide
+	pt: 'Português', // Portuguese, Native: 250 millions;
+	de: 'Deutsch', // German
+	ja: '日本語', // 	Japanese
+	zh: '中文', // Chinese
+	ar: 'العربية', // Arabic
+	el: 'ελληνικά', // Greek, Modern,
+	ko: '한국어', // Korean, Korea
+	pl: 'Polszczyzna', // Polish, Poland
+	ro: 'Română', // 	Romanian, Moldavian, Moldovan
+	sv: 'Svenska', // Swedish, 	Sweden, Finland, Estonia
+	tk: 'Türkmen', // Turkmen, 	Turkmenistan, Russia, Iran, Uzbekistan, Afghanistan, Iraq, Syria
+	tt: 'Татар теле', // Tatar, Russia, other post-Soviet states
+	cs: 'Čeština', // Czech, 	Czech Republic
+	nl: 'Nederlands', // Dutch, Belgium, Netherlands, France
+	uk: 'Українська', // Ukrainian
+	ru: 'Русский', // Russian
+	et: 'Eesti', // Estonian
+	fi: 'Suomi', // Finnish, Finland
+	ht: 'Kreyòl ayisyen', // 	Haitian, Haiti
+	hi: 'हिन्दी, हिंदी', // Hindi, India
+	hu: 'Magyar', // Hungarian, Hungary
+	ga: 'Gaeilge', // Irish, Ireland
+	kk: 'Қазақ', // 	Kazakh, Kazakstani language
+	mk: 'Македонски јазик', // Macedonian
+	no: 'Norsk', // Norwegian
+	he: 'עברית', // Hebrew, isrrael
 };
