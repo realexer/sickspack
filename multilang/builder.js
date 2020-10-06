54f33bce0f8dab5db682a5dda3932626f7a912d9
Alexey Skrypnik <unrealvsrealer@gmail.com>
2020-10-06 20:58:56 +0300

    Multilang.builder - refresh translation added.

--- multilang/builder.js
+++ multilang/builder.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import * as JsonTranslator from '../json-translator';
-import {flattenObject} from "../object-key-path";
+import {flattenObject, readByPath, writeByPath} from "../object-key-path";
 
 export const buildLangs = async (fromObject, langCodes, savePath) =>
 {
@@ -11,7 +11,43 @@ export const buildLangs = async (fromObject, langCodes, savePath) =>
 			console.debug(`Translating: [${lang}]`);
 
 			const translation = await JsonTranslator.translateObject(JSON.parse(JSON.stringify(fromObject)), lang);
-			await fs.promises.writeFile(path.resolve(__dirname, `${savePath}/${lang}.js`), `export default ${JSON.stringify(translation, null, '\t')}`);
+			await fs.promises.writeFile(path.resolve(__dirname, `${savePath}/${lang}.js`),
+				`export default ${JSON.stringify(translation, null, '\t')}`);
+
+		} catch (e) {
+			console.error(e);
+		}
+	}
+};
+
+export const refreshLangs = async (fromObject, translations, savePath) =>
+{
+	const langKeys = flattenObject(fromObject);
+
+	for(let lang in translations)
+	{
+		try {
+
+			console.debug(`Translating: [${lang}]`);
+
+			const langTranslation = translations[lang];
+
+			for(let keyPath in langKeys)
+			{
+				const originalValue = readByPath(fromObject, keyPath);
+
+				if(readByPath(langTranslation, keyPath) === undefined)
+				{
+					console.debug(`Translating: [${originalValue}]`);
+
+					const translatedValue = await JsonTranslator.translateObject(JSON.parse(JSON.stringify(originalValue)), lang);
+
+					writeByPath(langTranslation, keyPath, translatedValue);
+				}
+			}
+
+			await fs.promises.writeFile(path.resolve(__dirname, `${savePath}/${lang}.js`),
+				`export default ${JSON.stringify(langTranslation, null, '\t')}`);
 
 		} catch (e) {
 			console.error(e);
